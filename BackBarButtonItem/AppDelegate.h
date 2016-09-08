f8849948768adb6f563c88c03b10112c20c9f510
wuhaiwei <wuhaiwei0809@163.com>
2016-09-08 14:45:56 +0800

    Initial Commit

---
+++ BackBarButtonItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackBarButtonItem
+//
+//  Created by  wuhiwi on 16/9/8.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackBarButtonItem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BackBarButtonItem
+//
+//  Created by  wuhiwi on 16/9/8.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
